1be62a3ae738e244e81a18a49c0afb0e3e2cc9f2
Martín Núñez <mnunez@linkbgroup.com>
2023-12-27 19:16:05 -0600

    chore | Ejercicio con contadores usando v-on

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,8 @@
 <template>
   <h2>Counter!!</h2>
   <p> {{ counter }} <sup>2</sup> = {{ squaredCounter }}</p>
+  <button @click="increase()">+1</button>
+  <button @click="decrease()">-1</button>
 </template>
 
 <script>
@@ -14,6 +16,12 @@ export default {
     methods: {
         getSquaredValue() {
             return this.counter * this.counter
+        },
+        increase() {
+            this.counter = this.counter + 1
+        }, 
+        decrease() {
+            this.counter = this.counter - 1
         }
     },
     computed: {
@@ -26,5 +34,19 @@ export default {
 </script>
 
 <style>
+    button {
+        background-color: #64B687;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
 
+    button:hover {
+        background-color: #5AA67B;
+        transition: 0.3s ease-in-out;
+    }
 </style>
