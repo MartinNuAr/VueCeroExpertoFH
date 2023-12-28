e1b1a91e9468efaa8524115879a9e5729c198c2f
Martín Núñez <mnunez@linkbgroup.com>
2023-12-27 19:05:16 -0600

    chore | Inicio de App y uso de compute

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>Counter!!</h2>
+  <p> {{ counter }} <sup>2</sup> = {{ squaredCounter }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            counter: 5
+        }
+    },
+    methods: {
+        getSquaredValue() {
+            return this.counter * this.counter
+        }
+    },
+    computed: {
+        squaredCounter() {
+            return this.counter * this.counter
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
