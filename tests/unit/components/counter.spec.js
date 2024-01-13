f08940ce9b6baac0366080272bbb556f264ff1b7
Martín Núñez <mnunez@linkbgroup.com>
2024-01-12 19:38:18 -0600

    chore | Refactorización de pruebas unitarias

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -3,13 +3,18 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallowMount(Counter)
+    })
+
     // test('Debe de hacer match con el snapshot', () => {
     //     const wrapper = shallowMount(Counter)
     //     expect(wrapper.html()).toMatchSnapshot()
     // })
 
     test('<h2> debe tener el valor por defecto "Counter"', () => {
-        const wrapper = shallowMount(Counter)
         const h2 = wrapper.find('h2')
 
         // Revisa que exista la etiqueta
@@ -21,13 +26,29 @@ describe('Counter Component', () => {
     })
 
     test('El valor por defecto tiene que ser 10 en el <p>', () => {
-        const wrapper = shallowMount(Counter)
         const pTags = wrapper.findAll('p')
         const pTest = wrapper.find('[datatest-id="counter"]').text()
 
         // Revisa que la segunda etiqueta 'p' tenga el valor 100
         expect(pTags[0].text()).toBe('10')
         expect(pTest).toBe('10')
+    })
+
+    test('Debe incrementar en 1 el valor del contador', async() => {
+        const [increaseBtn, decreaseBtn] = wrapper.findAll('button')
+
+        // Incrementa en 3 el valor
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+
+        // Reduce en 2 el valor
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+
+        const value = wrapper.find('[datatest-id="counter"]').text()
+
+        expect(value).toBe('11')
     })
 
 })
