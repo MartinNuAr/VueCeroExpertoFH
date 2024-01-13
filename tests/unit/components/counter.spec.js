347e384359724f5c6fab37b30bcfa20a732ac5c9
Martín Núñez <mnunez@linkbgroup.com>
2024-01-12 18:54:33 -0600

    chore | Pruebas unitarias con etiquetas HTML

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -3,12 +3,21 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
-    test('Debe de hacer match con el snapshot', () => {
+    // test('Debe de hacer match con el snapshot', () => {
+    //     const wrapper = shallowMount(Counter)
+    //     expect(wrapper.html()).toMatchSnapshot()
+    // })
 
+    test('<h2> debe tener el valor por defecto "Counter"', () => {
         const wrapper = shallowMount(Counter)
-        
-        expect(wrapper.html()).toMatchSnapshot()
+        const h2 = wrapper.find('h2')
 
+        // Revisa que exista la etiqueta
+        expect(wrapper.find('h2').exists()).toBe(true)
+
+        // Revisa que el texto contenga 'Counter'
+        console.log(h2.text())
+        expect(h2.text()).toBe('Counter')
     })
 
 })
