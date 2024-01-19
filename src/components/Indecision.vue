caef697778f614779701a6edc7da5276be950c89
Martín Núñez <mnunez@linkbgroup.com>
2024-01-18 19:21:43 -0600

    chore | Se realizan pruebas de funciones y respuestas de API en componente de Indecision

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -30,16 +30,26 @@ export default {
     },
     methods: {
         async getAnswer() {
-            this.answer = 'Pensando...'
-
-            const { answer, image } = await fetch('https://yesno.wtf/api').then(res => res.json())
-
-            this.answer = answer == 'yes' ? 'Sí' : 'No'
-            this.img = image
+            try {
+                this.answer = 'Pensando...'
+    
+                const { answer, image } = await fetch('https://yesno.wtf/api').then(res => res.json())
+    
+                this.answer = answer == 'yes' ? 'Sí' : 'No'
+                this.img = image
+            } catch (error) {
+                console.log('IndecisionComponent: ', error)
+                this.answer = 'No se pudo cargar del API'
+                this.img = null
+            }
         }
     },  
     watch: {
         question(value, oldValue){
+            this.isValidQuestion = false
+
+            console.log({ value })
+
             if (!value.includes('?')) return
 
             this.isValidQuestion = true
